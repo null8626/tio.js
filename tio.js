b734bbf11ce0451b036fd910160d589d5bb473bf
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-05-23 14:34:48 +0430

    main: optimizations and improvements
    Optimized some documentation, method implementations and literals.

--- tio.js
+++ tio.js
@@ -2,7 +2,7 @@ const { request, get } = require('https');
 const { deflateRawSync, gunzipSync } = require('zlib');
 const { randomBytes } = require('crypto');
 const getRequestBody = (c, l) => deflateRawSync(Buffer.from([
-    `Vlang`, 1, l, 'VTIO_OPTIONS', 0, 'F.code.tio', c.length, `${c}F.input.tio`, 0, 'Vargs', 0, 'R'
+    'Vlang', 1, l, 'VTIO_OPTIONS', 0, 'F.code.tio', c.length, `${c}F.input.tio`, 0, 'Vargs', 0, 'R'
 ].join('\0'), 'binary'), { level: 9 });
 
 /**
@@ -11,21 +11,20 @@ const getRequestBody = (c, l) => deflateRawSync(Buffer.from([
  * @property {string} language The language used.
  * @property {number} realTime How long the code runs real time.
  * @property {number} userTime How long the code runs user time.
- * @property {number} sysTime How long the code runs sys. time.
+ * @property {number} sysTime How long the code runs system time.
  * @property {number} CPUshare The code's CPU share.
  * @property {number} exitCode The script's exit code.
  */
 
-var runURL = null;
-var languages = null;
-var defaultLanguage = 'javascript-node';
-var nextRequest = null;
+let runURL = null;
+let languages = null;
+let defaultLanguage = 'javascript-node';
+let nextRequest = null;
 
 /**
- * @async
  * Does a simple GET request to the TIO page. Used primarily for scraping.
- * @param {string} path Optional. The request path.
- * @returns {string} The request response.
+ * @param {string} [path] The request path.
+ * @returns {Promise<string>} The request response.
  */
 function requestText(path) {
     return new Promise(resolve => {
@@ -40,32 +39,32 @@ function requestText(path) {
 /**
  * @async
  * Handles if a language is available to use or not.
- * @returns {string} The parsed language.
+ * @returns {Promise<string>} The resolved language.
  */
 async function resolveLanguage(language) {
     if (!languages) {
         const response = JSON.parse(await requestText('/languages.json'));
-        languages = {};
         
-        for (const entry of Object.entries(response))
-            languages[entry[1].name] = entry[0];
+        languages = Object.fromEntries(
+            Object.entries(response).map(([K, V]) => [V.name, K])
+        );
     }
     
     if (languages[language]) return languages[language];
     else if (Object.values(languages).includes(language)) return language;
     
-    throw new TypeError(`Invalid language.`);
+    throw new TypeError('Invalid language.');
 }
 
 /**
  * @async
  * Gets the run URL by scraping the website's backend.
- * @returns {void}
+ * @returns {Promise<void>}
  */
 async function getRunURL() {
     if (runURL) return;
     const scrapeResponse = await requestText();
-    const frontendJSurl = scrapeResponse.match(/<script src="(\/static\/[0-9a-f]+\-frontend.js)" defer><\/script>/)[1];
+    const frontendJSurl = scrapeResponse.match(/<script src="(\/static\/[0-9a-f]+-frontend\.js)" defer><\/script>/)[1];
     const frontendJS = await requestText(frontendJSurl);
     runURL = frontendJS.match(/^var runURL = "\/cgi-bin\/static\/([^"]+)";$/m)[1];
 }
@@ -75,14 +74,14 @@ module.exports = Object.assign(
      * @async
      * Evaluates code through the TryItOnline API.
      * @param {string} code The code to run.
-     * @param {string} language The programming language to use. Uses the default language if not specified.
+     * @param {string} [language] The programming language to use. Uses the default language if not specified.
      * @returns {Promise<TioResponse>} The code response.
      */
     async (code, language) => {
-        if (!code || typeof code !== 'string')
-            throw new TypeError(`Code is missing.`);
+        if (typeof code !== 'string')
+            throw new TypeError("'code' must be a string");
         
-        const delayAmount = nextRequest ? nextRequest - Math.round(new Date().getTime() / 1000) : 0;
+        const delayAmount = nextRequest ? nextRequest - Math.round(Date.now() / 1000) : 0;
         if (delayAmount > 0)
             await new Promise(end => setTimeout(end, delayAmount * 1000));
         
@@ -95,7 +94,7 @@ module.exports = Object.assign(
             const randomStuff = randomBytes(16).toString('hex');
             
             request({
-                host: `tio.run`,
+                host: 'tio.run',
                 path: `/cgi-bin/static/${runURL}/${randomStuff}`,
                 method: 'POST'
             }, resp => {
@@ -108,26 +107,32 @@ module.exports = Object.assign(
             ));
         });
         
-        nextRequest = Math.round(new Date().getTime() / 1000) + 2;
+        nextRequest = Math.round(Date.now() / 1000) + 2;
         response = response.replace(new RegExp(response.slice(-16).replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), '');
         
         const split = response.split('\n');
-        const debug = split.slice(-5).map(x => Number(x.slice(11, ...(/[^\d]$/.test(x) ? [-2] : []))));
+        const [
+            realTime,
+            userTime,
+            sysTime,
+            CPUshare,
+            exitCode
+        ] = split.slice(-5).map(x => Number(x.slice(11, ...(/[^\d]$/.test(x) ? [-2] : []))));
         return {
             output: split.slice(0, -5).join('\n').trim(),
             language,
-            realTime: debug[0],
-            userTime: debug[1],
-            sysTime: debug[2],
-            CPUshare: debug[3],
-            exitCode: debug[4]
+            realTime,
+            userTime,
+            sysTime,
+            CPUshare,
+            exitCode
         };
     },
 {
     /**
      * @async
      * Sets the default language.
-     * @param {string} language Language to use from.
+     * @param {string} language Language to use as default.
      * @returns {Promise<void>}
      */
     setDefaultLanguage: async (language) => {
