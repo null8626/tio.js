1f390daed3a94f274fb856abd626e139be806109
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-05-25 13:40:28 +0430

    typings: fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,25 @@
-declare function tio(code: string, language?: string, timeout?: number): Promise<tio.TioResponse>;
-
-declare module tio {
+declare module 'tio.js' {
+  namespace tio {
     interface TioResponse {
-        output: string;
-        language: string;
-        timedOut: boolean;
-        realTime: number;
-        userTime: number;
-        sysTime: number;
-        CPUshare: number;
-        exitCode: number;
+      output: string;
+      language: string;
+      timedOut: boolean;
+      realTime: number;
+      userTime: number;
+      sysTime: number;
+      CPUshare: number;
+      exitCode: number;
     }
     
-    export function setDefaultLanguage(language: string): Promise<void>;
-    export function getDefaultLanguage(): string;
-    export function setDefaultTimeout(timeout: number | null): void;
-    export function getDefaultTimeout(): number | null;
-    export function languages(): Promise<string[]>;
-    export const version: string;
+    function setDefaultLanguage(language: string): Promise<void>;
+    function getDefaultLanguage(): string;
+    function setDefaultTimeout(timeout: number | null): void;
+    function getDefaultTimeout(): number | null;
+    function languages(): Promise<string[]>;
+    const version: string;
+  }
+  
+  function tio(code: string, language?: string, timeout?: number): Promise<tio.TioResponse>;
+  
+  export = tio;
 }
-
-export = tio;
