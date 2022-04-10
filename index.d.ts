56265b9b88a5d1ddb4619761ccd95e65303c3876
null <60427892+vierofernando@users.noreply.github.com>
2022-04-10 19:29:31 +0700

    fix: timeout instead of defaultTimeout

--- index.d.ts
+++ index.d.ts
@@ -9,11 +9,5 @@ export interface TioResponse {
     readonly exitCode: number;
 }
 export declare type Option<T> = T | undefined | null;
-declare function fetchLanguages(): Promise<string[]>;
-declare function setDefaultLanguage(language: string): Promise<void>;
-declare function getDefaultLanguage(): string;
-declare function setDefaultTimeout(timeout: Option<number>): void;
-declare function getDefaultTimeout(): Option<number>;
-declare const version: string;
-export { fetchLanguages as languages, setDefaultLanguage, getDefaultLanguage, setDefaultTimeout, getDefaultTimeout, version };
-export default function tioRun(code: string, language: Option<string>, timeout: Option<number>): Promise<TioResponse>;
+declare function tioRun(code: string, language: Option<string>, timeout: Option<number>): Promise<TioResponse>;
+export default tioRun;
