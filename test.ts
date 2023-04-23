52853f1b785ce58316ec764d6fbdfd997be965be
null8626 <vierofernando9@gmail.com>
2023-04-23 23:01:43 +0700

    *: no more jest!

--- test.ts
+++ test.ts
@@ -1,21 +1,24 @@
-import tio from '../src/index'
+import { strictEqual } from 'node:assert'
+import { it } from 'node:test'
+
+import tio from './dist/index.js'
 
 it('outputs Hello, World in JavaScript', async () => {
   const { output } = await tio("console.log('Hello, World!');")
 
-  expect(output).toBe('Hello, World!\n')
+  strictEqual(output, 'Hello, World!\n')
 })
 
 it('outputs Hello, World in Python 3', async () => {
   tio.defaultLanguage = 'python3'
   const { output } = await tio("print('Hello, World!')")
 
-  expect(output).toBe('Hello, World!\n')
+  strictEqual(output, 'Hello, World!\n')
 })
 
 it('surpresses an infinite loop', async () => {
   const { output, timedOut } = await tio('for (;;);', 'javascript-node', 2000)
 
-  expect(timedOut).toBe(true)
-  expect(output).toBe('Request timed out after 2000ms')
+  strictEqual(timedOut, true)
+  strictEqual(output, 'Request timed out after 2000ms')
 })
